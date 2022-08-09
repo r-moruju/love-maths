e6a5d5fc1ee17526273f483d706996d7057891e0
R Moruju <morujurazvan@gmail.com>
2022-08-09 19:24:09 +0000

    Add subtract function

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,6 +33,8 @@ function runGame(gameType) {
         displayAdditionQuestion(num1, num2);
     } else if (gameType === "multiply") {
         displayMultiplyQuestion(num1, num2);
+    } else if (gameType === "substract") {
+        diaplaySubstractQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -73,6 +75,8 @@ function calculateCorrectAnswer() {
         return[operand1 + operand2, "addition"];
     } else if (operator === "x") {
         return[operand1 * operand2, "multiply"];
+    } else if ( operator === "-") {
+        return[operand1 - operand2, "substract"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -104,8 +108,11 @@ function displayAdditionQuestion(operand1, operand2) {
     document.getElementById('operator').textContent = "+";
 }
 
-function diaplaySubstractQuestion() {
+function diaplaySubstractQuestion(operand1, operand2) {
 
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "-";
 }
 
 function displayMultiplyQuestion(operand1, operand2) {
