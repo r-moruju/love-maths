57d04e3745b7c71041be728124f0c7bccb5fef28
R Moruju <morujurazvan@gmail.com>
2022-08-09 19:39:46 +0000

    Add favicon and last tidy ups

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,12 @@ document.addEventListener("DOMContentLoaded", function() {
         })
     }
 
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if ( event.key === "Enter") {
+            checkAnswer();
+        }
+    })
+
     runGame("addition");
 })
 
@@ -24,6 +30,9 @@ document.addEventListener("DOMContentLoaded", function() {
  */
 
 function runGame(gameType) {
+
+    document.getElementById('answer-box').value = "";
+    document.getElementById('answer-box').focus();
 
     // Creates two random numbers from 1 to 25
     let num1 = Math.floor(Math.random() * 25) + 1;
